75c1f98f1938f27946fb72b10666d580e84b0251
mcharris <mcharris@adobe.com>
2019-10-03 15:23:04 -0600

    Add base parser and test

--- src/test/scala/com/mcguffin/darnassus/parser/DslParserSpec.scala
+++ src/test/scala/com/mcguffin/darnassus/parser/DslParserSpec.scala
@@ -1,5 +1,23 @@
 package com.mcguffin.darnassus.parser
 
-class DslParserSpec {
+import org.scalatest.{Matchers, WordSpec}
 
+class DslParserSpec extends WordSpec
+  with Matchers {
+
+  val testDsl =
+    """
+      |{
+      |  "source": "rabbit",
+      |  "sink": "hbase"
+      |}
+      |""".stripMargin
+
+  "DslParser" should {
+    "parse the source and sink" in {
+      val parsed = DslParser.parse(testDsl);
+      parsed.source shouldEqual "rabbit"
+      parsed.sink shouldEqual "hbase"
+    }
+  }
 }
